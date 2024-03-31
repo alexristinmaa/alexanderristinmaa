acbe8b4222e2a85d031466d742f26eaf10328aa6
matrixthecow <alexander.ristinmaa@icloud.com>
2024-03-31 17:47:09 +0200

    Cats

--- alexanderristinmaa/app/(homepage)/page.tsx
+++ alexanderristinmaa/app/(homepage)/page.tsx
@@ -1,10 +1,13 @@
+// nextjs
+import Link from 'next/link'
+
 // style
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div>
-        <p>Hej. Jag heter Alexander Ristinmaa och detta är min hemsida. Om du vågar kolla runt kan du säkert hitta vad du letar efter.</p>
+        <p>Hej. Jag heter Alexander Ristinmaa och detta är min hemsida. Om du vågar kolla runt kan du säkert hitta v<Link href='/hemligt'>a</Link>d du letar efter.</p>
     </div>
   );
 }
---
+++ alexanderristinmaa/app/hemligt/page.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+// nextjs
+import { useState, useEffect } from 'react';
+import Script from 'next/script';
+
+// style
+import styles from './page.module.css';
+
+// misc
+import fcontent from './catimation';
+
+export default function Home() {
+  const [catimationState, setCatimation] = useState(fcontent[0]);
+
+  let animator = (frame : number) => {
+    setCatimation(fcontent[frame]);
+    setTimeout(animator, 100, frame < fcontent.length ? frame + 1 : 0);
+  }
+
+  useEffect(() => {
+    animator(0);
+  }, []);
+
+  return (
+    <div>
+      <p>Detta skulle ju vara hemligt vad fan?</p>
+      <br /><br /><br /><br /><br />
+      <p style={{whiteSpace: 'pre-wrap'}}>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,,uod8B8bou,,.<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..,uod8BBBBBBBBBBBBBBBBRPFT?l!i:.<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;,=m8BBBBBBBBBBBBBBBRPFT?!||||||||||||||<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;!...:!TVBBBRPFT||||||||||!!^^""'   ||||<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;!.......:!?|||||!!^^""'            ||||<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;!.........||||                     ||||<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;!.........||||  ##                 ||||<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;!.........||||                     ||||<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;!.........||||                     ||||<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;!.........||||                     ||||<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;!.........||||                     ||||<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`.........||||                    ,||||<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.;.......||||               _.-!!|||||<br/>
+&nbsp;&nbsp;&nbsp;.,uodWBBBBb.....||||       _.-!!|||||||||!:'<br/>
+!YBBBBBBBBBBBBBBb..!|||:..-!!|||||||!iof68BBBBBb....<br/>
+!..YBBBBBBBBBBBBBBb!!||||||||!iof68BBBBBBRPFT?!::   `.<br/>
+!....YBBBBBBBBBBBBBBbaaitf68BBBBBBRPFT?!:::::::::     `.<br/>
+!......YBBBBBBBBBBBBBBBBBBBRPFT?!::::::;:!^"`;:::       `.<br/>
+!........YBBBBBBBBBBRPFT?!::::::::::^''...::::::;         iBBbo.<br/>
+`..........YBRPFT?!::::::::::::::::::::::::;iof68bo.      WBBBBbo.<br/>
+&nbsp;&nbsp;`..........:::::::::::::::::::::::;iof688888888888b.     `YBBBP^'<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;`........::::::::::::::::;iof688888888888888888888b.     `<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`......:::::::::;iof688888888888888888888888888888b.<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`....:::;iof688888888888888888888888888888888899fT!<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`..::!8888888888888888888888888888888899fT|!^"'<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`' !!988888888888888888888888899fT|!^"'<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`!!8888888888888888899fT|!^"'<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`!988888888899fT|!^"'<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`!9899fT|!^"'<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`!^"'
+      </p>
+      <br /><br /><br /><br />
+      <p>Varför snokar du?</p>
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      Okej, bajs
+      <pre id={styles.catimator} dangerouslySetInnerHTML={{__html: catimationState }}></pre>
+    </div>
+  );
+}
