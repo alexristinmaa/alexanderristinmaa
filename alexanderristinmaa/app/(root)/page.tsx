c7951e2fb17ec38491a2246dc3d0ba308f2796b1
matrixthecow <alexander.ristinmaa@icloud.com>
2024-12-09 00:45:10 +0100

    Navigator fix?

--- alexanderristinmaa/app/(root)/page.tsx
+++ alexanderristinmaa/app/(root)/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import dynamic from 'next/dynamic';
 import { isDictKey } from '../[lang]/dictionaries'
 
 import { redirect } from 'next/navigation';
 
-export default function Root() {
-    for(let language of navigator.languages) {
+export default function Root() {    
+    const nav = navigator || {languages: ["en"]};
+    for(let language of nav.languages) {
         let lang = language.split("-")[0];
 
         if(isDictKey(lang)) redirect(`${lang}`)
